ba71fe548f0b8ac58b56af0ba8d7c5b6a54dd282
sadiamim <sadiamim646@gmail.com>
2026-03-02 11:27:50 +0600

    Fix CI Chrome session issue with WebDriverManager

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -32,9 +32,9 @@ public class BaseTest {
         options.addArguments("--window-size=1920,1080");
 
         driver = new ChromeDriver(options);
-
 //        driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.saucedemo.com/");
     }
 
 //    @Attachment(value = "Screenshot on Failure", type = "image/png")
